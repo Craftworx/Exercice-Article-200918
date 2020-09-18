e313067e42409402eca8843b597c710a71993a07
Eric <craftworx@protonmail.com>
2020-09-18 18:44:28 +0200

    Initial Commit

---
+++ Exercice Article 200918/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Exercice Article 200918
+//
+//  Created by Al on 18/09/2020.
+//  Copyright © 2020 Cool Apps. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
